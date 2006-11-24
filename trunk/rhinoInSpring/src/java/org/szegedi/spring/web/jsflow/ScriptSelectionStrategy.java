1ae30f542a318f95d5f80d79dda503b3acc0340c
szegedia <szegedia@50b3449b-d913-0410-a172-c4d8d39ce161>
2006-11-24 13:20:10 +0000

    Allow it to throw a ModelAndViewDefiningException

--- trunk/rhinoInSpring/src/java/org/szegedi/spring/web/jsflow/ScriptSelectionStrategy.java
+++ trunk/rhinoInSpring/src/java/org/szegedi/spring/web/jsflow/ScriptSelectionStrategy.java
@@ -2,6 +2,8 @@ package org.szegedi.spring.web.jsflow;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.web.servlet.ModelAndViewDefiningException;
+
 /**
  * Interface for objects that select a script for a particular initial HTTP 
  * request.
@@ -17,7 +19,9 @@ public interface ScriptSelectionStrategy
      * @return the path of the script. null can be returned to indicate that
      * this strategy is unable to select a script (i.e. because some data is
      * missing in the request). The controller will respond to this by sending
-     * back a HTTP 400 "Bad Request" status.
+     * back a HTTP 400 "Bad Request" status. Alternatively, the strategy can
+     * throw an instance of {@link ModelAndViewDefiningException}.
      */
-    public String getScriptPath(HttpServletRequest request);
+    public String getScriptPath(HttpServletRequest request) 
+    throws ModelAndViewDefiningException;
 }
