44ce1640b0720cb4790493fc3f9132e6b093099f
szegedia <szegedia@50b3449b-d913-0410-a172-c4d8d39ce161>
2007-02-15 10:06:31 +0000

    Added a PropertyResourceRepresentation

---
+++ trunk/rhinoInSpring/src/java/org/szegedi/spring/core/io/PropertyResourceRepresentation.java
@@ -0,0 +1,29 @@
+package org.szegedi.spring.core.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.springframework.core.io.Resource;
+
+/**
+ * A resource representation that treats the resource as a Java properties file
+ * and represents the resource with a Properties object initialized with those 
+ * properties.
+ * @author Attila Szegedi
+ * @version $Id: $
+ */
+public class PropertyResourceRepresentation extends ResourceRepresentation
+{
+    public PropertyResourceRepresentation(Resource resource)
+    {
+        super(resource);
+    }
+
+    protected Object loadRepresentation(InputStream in) throws IOException
+    {
+        Properties properties = new Properties();
+        properties.load(in);
+        return properties;
+    }
+}
